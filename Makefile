11b22d34447768cf47eaad1ed8c3f75c12dc6c9f
Alexander Shorin <kxepal@gmail.com>
2014-01-05 06:49:36 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,12 @@
+.PHONY: help install test
+
+help:
+	@echo "Please use \`make <target>' where <target> is one of"
+	@echo "  install    to install tasman with all dependencies"
+	@echo "  test       to run test suite"
+
+install:
+	pip install -e ./ -r requirements.txt
+
+test:
+	@python2 tasman/tests/__init__.py
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+#
+
+from distutils.core import setup
+
+setup(
+    name='tasman',
+    version='0.1',
+    packages=['tasman', 'tasman.tests'],
+    url='http://svn.miranda-ng.org/tasman',
+    license='BSD',
+    author='Alexander Shorin',
+    author_email='kxepal@gmail.com',
+    description='XMPP bot for Miranda NG conference based on XMPPFlask'
+)
